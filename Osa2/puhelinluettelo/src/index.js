359a3ef261ea55e04e141cc836ee03a4c992bbc9
Pyry Pohjalainen <pyry@MacBook-Pro.local>
2020-11-28 15:08:57 +0200

    2.11 tehty

--- Osa2/puhelinluettelo/src/index.js
+++ Osa2/puhelinluettelo/src/index.js
@@ -1,17 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 import AddPerson from './components/addPerson'
 import FilterPersons from './components/filter'
 import Person from './components/person'
-
-
-
-
+import { valueToNode } from '@babel/types'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '12345678' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ findName, setFindName ] = useState('')
@@ -32,6 +28,14 @@ const App = () => {
     }
   }
   
+  useEffect(() => {
+    console.log('useEffect')
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response)
+        setPersons(response.data)
+      })
+  },[])
 
   const editnewName = (event) => {
     setNewName(event.target.value)
