72a7cea5596881bf0f43b5eb4d74ac7c6363dea6
Pyry Pohjalainen <pyry@MacBook-Pro.local>
2020-12-04 16:09:12 +0200

    3.4-3.6 tehty

--- Osa3/puhelinluettelo-backend/index.js
+++ Osa3/puhelinluettelo-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
       {
         "name": "Arto Hellas",
@@ -48,6 +50,43 @@ let persons = [
         } else {
             res.status(404).end()
         }
+        
+    })
+
+    app.delete(`/api/persons/:id`,(req, res) => {
+        const id = Number(req.params.id)
+        persons = persons.filter(p => p.id !== id)
+        res.status(204).end()
+        
+    })
+
+    const generateId = () => {
+        return Math.floor(Math.random() * Math.floor(10000))
+      }
+
+    app.post(`/api/persons`,(req, res) => {
+        const body = req.body
+        if (persons.filter(person => person.name === body.name).length > 0) {
+            return res.status(400).json({
+                error: 'name must be unique'
+            })
+        } else if (!body.name) {
+            return res.status(400).json({
+                error: 'name must be given'
+            })
+        }
+        if (persons.filter(person => person.number === body.number).length > 0) {
+            return res.status(400).json({
+                error: 'number must be unique'
+            })
+        } 
+        const person = {
+            name: body.name,
+            number: body.number,
+            id: generateId(),
+        }
+        persons = persons.concat(person)
+        res.json(person)
         
     })
 
